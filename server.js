d05f0bb810721961b3d9bb578feaa8cab4977280
kunal786ks <136335466+kunal786ks@users.noreply.github.com>
2023-07-31 17:58:10 +0530

    df

--- server.js
+++ server.js
@@ -17,10 +17,24 @@ mongoose.connect(process.env.CONN_STR,
     console.log('An error Occurred')
 })
 
+const movieSchema=new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true,'Name is Mandatory'],
+    },
+    description:String,
+    duration:{
+        type:Number,
+        unique:true,
+        required:[true,'Duration is required feild'], //required is like validator
+    },
+    ratings:{
+        type:Number,
+        default:4.5,
+    },
+})//based on this schema we can create a model
 
-
-
-
+const movie=mongoose.model('Movie',movieSchema);//here we created the model
 
 const port = process.env.PORT;
 app.listen(port, () => {
